955d00ca5b92ec7cfd7778d87a5cff01e0f8b6eb
hrodmn <henry.rodman@gmail.com>
2025-03-01 06:52:54 -0600

    modernize dev setup

--- infra/AuthStack.py
+++ infra/AuthStack.py
@@ -4,17 +4,18 @@ from typing import Any, Dict, Optional, Sequence
 
 
 from aws_cdk import (
-    aws_iam as iam,
-    aws_cognito as cognito,
-    aws_cognito_identitypool_alpha as cognito_id_pool,
-    aws_s3 as s3,
-    aws_secretsmanager as secretsmanager,
-    aws_ssm as ssm,
     CfnOutput,
-    custom_resources as cr,
     RemovalPolicy,
     SecretValue,
     Stack,
+    aws_cognito as cognito,
+    aws_cognito_identitypool_alpha as cognito_id_pool,
+    aws_iam as iam,
+    aws_kms as kms,
+    aws_s3 as s3,
+    aws_secretsmanager as secretsmanager,
+    aws_ssm as ssm,
+    custom_resources as cr,
 )
 from constructs import Construct
 
@@ -25,7 +26,9 @@ class BucketPermissions(str, Enum):
 
 
 class AuthStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self, scope: Construct, construct_id: str, ade_iam_role: str, **kwargs
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
         self.userpool = self._create_userpool()
         self.domain = self._add_domain(self.userpool)
@@ -77,7 +80,6 @@ class AuthStack(Stack):
         )
 
     def _create_userpool(self) -> cognito.UserPool:
-
         return cognito.UserPool(
             self,
             "userpool",
@@ -96,7 +98,6 @@ class AuthStack(Stack):
         userpool: cognito.UserPool,
         auth_provider_client: cognito.UserPoolClient,
     ) -> cognito_id_pool.IdentityPool:
-
         userpool_provider = cognito_id_pool.UserPoolAuthenticationProvider(
             user_pool=userpool,
             user_pool_client=auth_provider_client,
@@ -114,7 +115,8 @@ class AuthStack(Stack):
             role_mappings=[
                 cognito_id_pool.IdentityPoolRoleMapping(
                     provider_url=cognito_id_pool.IdentityPoolProviderUrl.user_pool(
-                        f"cognito-idp.{stack.region}.{stack.url_suffix}/{userpool.user_pool_id}:{auth_provider_client.user_pool_client_id}"
+                        f"cognito-idp.{stack.region}.{stack.url_suffix}/"
+                        f"{userpool.user_pool_id}:{auth_provider_client.user_pool_client_id}"
                     ),
                     use_token=True,
                     mapping_key="userpool",
@@ -131,7 +133,9 @@ class AuthStack(Stack):
 
         domain = userpool.add_domain(
             "cognito-domain",
-            cognito_domain=cognito.CognitoDomainOptions(domain_prefix=stack_name.lower()),
+            cognito_domain=cognito.CognitoDomainOptions(
+                domain_prefix=stack_name.lower()
+            ),
         )
 
         CfnOutput(
@@ -147,7 +151,6 @@ class AuthStack(Stack):
         self,
         client: cognito.UserPoolClient,
     ) -> str:
-
         describe_cognito_user_pool_client = cr.AwsCustomResource(
             self,
             f"describe-{client.to_string()}",
@@ -251,7 +254,6 @@ class AuthStack(Stack):
         name: Optional[str] = None,
         replica_regions: Optional[Sequence[str]] = None,
     ) -> cognito.UserPoolClient:
-
         client = self.userpool.add_client(
             service_id,
             auth_flows=cognito.AuthFlow(user_password=True),
@@ -310,7 +312,8 @@ class AuthStack(Stack):
             "secret-parameter",
             parameter_name=f"/{service_id}/secret",
             string_value=secret.secret_arn,
-            description=f"ARN of the secret containing credentials for {service_id} service client",
+            description=f"ARN of the secret containing credentials for {service_id} "
+            "service client",
             tier=ssm.ParameterTier.STANDARD,
         )
 
@@ -332,7 +335,7 @@ class AuthStack(Stack):
         description: str,
         bucket_permissions: Dict[str, BucketPermissions],
     ) -> cognito.CfnUserPoolGroup:
-
+        identity_pool_id = self.identitypool.identity_pool_id
         role = iam.Role(
             self,
             f"{group_name}_role",
@@ -341,7 +344,7 @@ class AuthStack(Stack):
                 assume_role_action="sts:AssumeRoleWithWebIdentity",
                 conditions={
                     "StringEquals": {
-                        "cognito-identity.amazonaws.com:aud": self.identitypool.identity_pool_id
+                        "cognito-identity.amazonaws.com:aud": identity_pool_id
                     }
                 },
             ),
